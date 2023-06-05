cccb0bf1cd89e982ca6cced0fe49a57c11433a42
Almut-Eisele <81420081+Almut-Eisele@users.noreply.github.com>
2023-06-05 10:24:57 +0200

    Update calculate_correlations.R
    change default parameters

--- GEMLI_package_v0/R/calculate_correlations.R
+++ GEMLI_package_v0/R/calculate_correlations.R
@@ -1,8 +1,8 @@
-calculate_correlations <- function(data, fast = TRUE)
+calculate_correlations <- function(data, fast = FALSE)
 {
   tmp = t(apply(data, 1, rank)); tmp = as.matrix(tmp); tmp = tmp - rowMeans(tmp); tmp = tmp / sqrt(rowSums(tmp^2))
   if(fast == TRUE){
-  r = fastCor(t(tmp), nSplit = 10, upperTri = TRUE, optBLAS = TRUE)}
+  r = fastCor(t(tmp), nSplit = 10, upperTri = TRUE, optBLAS = TRUE, verbose=FALSE)}
   else{
   r = tcrossprod(tmp)}
   diag(r) <- 0
