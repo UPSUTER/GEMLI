43fed138a5a1c0485db4819c7c64b4fc9b905c2e
Almut-Eisele <81420081+Almut-Eisele@users.noreply.github.com>
2023-10-12 15:32:38 +0200

    Update predict_lineages_with_known_markers.R

--- GEMLI_package_v0/R/predict_lineages_with_known_markers.R
+++ GEMLI_package_v0/R/predict_lineages_with_known_markers.R
@@ -1,4 +1,4 @@
-predict_lineages_with_known_markers <- function(GEMLI_items, repetitions=100, sample_size=(2/3), desired_cluster_size=c(2,3), N=2, fast=FALSE)
+predict_lineages_with_known_markers <- function(GEMLI_items, repetitions=100, sample_size=(2/3), lineage_size=c(2,3), fast=FALSE)
 {
   norm_data = norm_data = GEMLI_items[['gene_expression']]
   marker_genes = GEMLI_items[['known_markers']]
@@ -9,7 +9,7 @@ predict_lineages_with_known_markers <- function(GEMLI_items, repetitions=100, sa
     cell_clusters = quantify_clusters_iterative(norm_data, marker_genes_sample, N=2, fast=FALSE)
     cell_clusters_unique_name = cell_clusters; for (colname in 1:ncol(cell_clusters)){cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname] = paste0(colname,'_',cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname])}
     clustersize_dict = table(cell_clusters_unique_name)
-    smallest_clusters = names(clustersize_dict)[clustersize_dict %in% desired_cluster_size]
+    smallest_clusters = names(clustersize_dict)[clustersize_dict %in% lineage_size]
     best_prediction = data.matrix(matrix(F, nrow=ncol(norm_data), ncol=ncol(norm_data))); rownames(best_prediction) = colnames(norm_data); colnames(best_prediction) = colnames(norm_data)
     for (cluster in smallest_clusters){cells_in_cluster = rownames(best_prediction)[rowSums(cell_clusters_unique_name==cluster, na.rm=T)>0]; best_prediction[cells_in_cluster,cells_in_cluster] <- T}
     diag(best_prediction) = F
