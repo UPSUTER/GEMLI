b9e45c380a456075c871ac5b7475da6fa139e8ef
Almut-Eisele <81420081+Almut-Eisele@users.noreply.github.com>
2023-10-12 15:30:42 +0200

    Update predict_lineages.R

--- GEMLI_package_v0/R/predict_lineages.R
+++ GEMLI_package_v0/R/predict_lineages.R
@@ -1,4 +1,4 @@
-predict_lineages <- function(GEMLI_items, repetitions=100, sample_size=(2/3), desired_cluster_size=c(2,3), N=2, fast=FALSE) # check
+predict_lineages <- function(GEMLI_items, repetitions=100, sample_size=(2/3), lineage_size=c(2,3), fast=FALSE) # check
 {
   data_matrix = GEMLI_items[['gene_expression']]
   marker_genes = potential_markers(data_matrix)
@@ -9,7 +9,7 @@ predict_lineages <- function(GEMLI_items, repetitions=100, sample_size=(2/3), de
     cell_clusters = quantify_clusters_iterative(data_matrix, marker_genes_sample, N=2, fast)
     cell_clusters_unique_name = cell_clusters; for (colname in 1:ncol(cell_clusters)){cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname] = paste0(colname,'_',cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname])}
     clustersize_dict = table(cell_clusters_unique_name)
-    smallest_clusters = names(clustersize_dict)[clustersize_dict %in% desired_cluster_size]
+    smallest_clusters = names(clustersize_dict)[clustersize_dict %in% lineage_size]
     best_prediction = data.matrix(matrix(F, nrow=ncol(data_matrix), ncol=ncol(data_matrix))); rownames(best_prediction) = colnames(data_matrix); colnames(best_prediction) = colnames(data_matrix)
     for (cluster in smallest_clusters){cells_in_cluster = rownames(best_prediction)[rowSums(cell_clusters_unique_name==cluster, na.rm=T)>0]; best_prediction[cells_in_cluster,cells_in_cluster] <- T}
     diag(best_prediction) = F
