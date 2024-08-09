1e37b1423c717cc83789fdba771733d2384f5f3d
evarist <evarist.planet@nexco.ch>
2024-08-09 09:29:16 +0200

    remove r cmd check file

--- GEMLI_package_v1/R/calculate_correlations.R
+++ GEMLI_package_v1/R/calculate_correlations.R
@@ -1,11 +1,7 @@
-calculate_correlations <- function(data, fast = FALSE)
+calculate_correlations <- function(data)
 {
     tmp = t(apply(data, 1, rank)); tmp = as.matrix(tmp); tmp = tmp - rowMeans(tmp); tmp = tmp / sqrt(rowSums(tmp^2))
-    if(fast == TRUE){
-        r = fastCor(t(tmp), nSplit = 10, upperTri = TRUE, optBLAS = TRUE, verbose=FALSE)}
-    else{
-        r = tcrossprod(tmp)
-    }
+    r = tcrossprod(tmp)
     diag(r) <- 0
     return(r)
 }
